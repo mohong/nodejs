68cdf04a876f56215c8a458c79db155947e69a73
mohong <arthas_qin@qq.com>
2016-10-17 19:06:45 +0800

    成功遍历一个网页DOM

--- demo/http1.js
+++ demo/http1.js
@@ -1,6 +1,8 @@
 //发送http获取网页内容
+'use strict';
 
 const request = require('request');
+const cheerio = require('cheerio');
 const option = {
     url: 'http://127.0.0.1:8080/jQuerytest/index.html',
     headers: {
@@ -10,10 +12,10 @@ const option = {
 
 function callback(error,response,body) {
     if (!error && response.statusCode == 200){
-        console.log(body);
+      let $ = cheerio.load(body);
+        let lis = $('li');
+        console.log(lis);
     }
 }
 
 request(option,callback);
-
-
