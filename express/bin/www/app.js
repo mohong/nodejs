e24ae3b25e9da519408d2f8b2ec86c15eb93e4ce
mohong <522916411@qq.com>
2016-10-30 12:01:31 +0800

    写入问答列表json数据

--- express/bin/www/app.js
+++ express/bin/www/app.js
@@ -20,26 +20,50 @@ app.get('/category',function (req,res) {
 
 app.get('/questions',function (req,res) {
     res.status(200);
-    res.json([
-        {
-            id: 1,
-            title: 't1',
-            asker: 'mohong',
-            course: 'nodejs',
-            'last-replay': Date.now(),
-            replay: 1
-        },
-        {
-            id: 2,
-            title: 't2',
-            asker: 'mohong2',
-            course: 'nodejs2',
-            'last-replay': Date.now(),
-            replay: 2
-        }
-    ]);
+    res.json(questions);
 });
 
+app.get('/questions/all',function (req,res) {
+    res.status(200);
+    res.json(questions);
+});
+
+app.get('/questions/resolved',function (req,res) {
+    res.status(200);
+    res.json(questions.filter(function (q) {
+        return q && q.state == 'resolved';
+    }));
+});
+
+app.get('/questions/unresolved',function (req,res) {
+    res.status(200);
+    res.json(questions.filter(function (q) {
+        return q && q.state == 'unresolved';
+    }));
+});
 
 app.listen(3000);
 console.log('server already start');
+
+
+//回答问题列表数据
+var questions = [
+    {
+        id: 1,
+        title: 't1',
+        asker: 'mohong',
+        course: 'nodejs',
+        'last-replay': Date.now(),
+        replay: 1,
+        state: 'resolved'
+    },
+    {
+        id: 2,
+        title: 't2',
+        asker: 'mohong2',
+        course: 'nodejs2',
+        'last-replay': Date.now(),
+        replay: 2,
+        state: 'unresolved'
+    }
+];
